46a1aa83013543ca369b8bf71b59ca4e5f84fbf3
pattmaws <patmawatisuminar@gmail.com>
2022-11-13 14:30:17 +0700

    second commit

--- app/src/main/java/com/example/desainui/MainActivity.java
+++ app/src/main/java/com/example/desainui/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.desainui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    public void pindahkehistory(View view) {
+        Intent intent = new Intent(MainActivity.this, history.class);
+        startActivity(intent);
     }
 }
